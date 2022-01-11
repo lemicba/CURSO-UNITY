63554cba59af39a031d993c1ae6eaf0be118ca8d
lemicba <lemicba@gmail.com>
2022-01-10 21:13:16 -0300

    Desafio complementario 1

--- Assets/Scripts/CanyonController.cs
+++ Assets/Scripts/CanyonController.cs
@@ -12,18 +12,39 @@ public class CanyonController : MonoBehaviour
     {
 
     }
-
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Disparo();
         }
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            DisparoFinal(2);
+        }
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            DisparoFinal(3);
+        }
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            DisparoFinal(4);
+        }
     }
 
     void Disparo()
     {
         Instantiate(Bala, DisparoPosicionInicial.position, DisparoPosicionInicial.rotation);
+    }
+
+    void DisparoFinal(int numeroDisparos)
+    {
+        float tiempo = 0f;
+
+        for (int i = 0; i < numeroDisparos; i++)
+        {
+            Invoke("Disparo", (tiempo));
+            tiempo = tiempo + 0.1f;
+        }
     }
 }
