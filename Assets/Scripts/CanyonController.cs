fa58eefd386ace7351feb77ec78987bf9024e7c9
lemicba <lemicba@gmail.com>
2022-01-12 23:11:20 -0300

    first commit

--- Assets/Scripts/CanyonController.cs
+++ Assets/Scripts/CanyonController.cs
@@ -6,45 +6,35 @@ public class CanyonController : MonoBehaviour
 {
     public Transform DisparoPosicionInicial;
     public GameObject Bala;
+    public float timeToShoot = 1f;
+    private float timeToShootLeft;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        ResetTimer();
+        //InvokeRepeating("Disparo", fireDelay, spawnInterval);
     }
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Disparo();
-        }
-        if (Input.GetKeyDown(KeyCode.J))
-        {
-            DisparoFinal(2);
-        }
-        if (Input.GetKeyDown(KeyCode.K))
-        {
-            DisparoFinal(3);
-        }
-        if (Input.GetKeyDown(KeyCode.L))
-        {
-            DisparoFinal(4);
-        }
+        Temporalizador();
     }
 
     void Disparo()
     {
         Instantiate(Bala, DisparoPosicionInicial.position, DisparoPosicionInicial.rotation);
     }
-
-    void DisparoFinal(int numeroDisparos)
+    private void ResetTimer()
     {
-        float tiempo = 0f;
-
-        for (int i = 0; i < numeroDisparos; i++)
+        timeToShootLeft = timeToShoot;
+    }
+    private void Temporalizador()
+    {
+        timeToShootLeft -= Time.deltaTime;
+        if (timeToShootLeft <= 0)
         {
-            Invoke("Disparo", (tiempo));
-            tiempo = tiempo + 0.1f;
+            Disparo();
+            ResetTimer();
         }
     }
 }
--- Assets/Scripts/bulletController.cs
+++ Assets/Scripts/bulletController.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class bulletController : MonoBehaviour
 {
-    public float speed = 35f;
+    public float speed = 10f;
     public Vector3 direction = Vector3.forward;
     public int damage = 1;
+    public float timeToDestroy = 3f;
+    private Vector3 nuevaEscala = new Vector3(1f, 1f, 1f);
     void Start()
     {
         
@@ -14,10 +16,29 @@ public class bulletController : MonoBehaviour
     void Update()
     {
         MoveBullet();
+        BulletDestroy();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ScaleTransform();
+        }
     }
 
     void MoveBullet()
     {
         transform.Translate(speed * Time.deltaTime * direction);
     }
+    void ScaleTransform()
+    {
+        this.gameObject.transform.localScale += nuevaEscala;
+    }
+    void BulletDestroy()
+    {
+        timeToDestroy -= Time.deltaTime;
+        if (timeToDestroy <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
 }
